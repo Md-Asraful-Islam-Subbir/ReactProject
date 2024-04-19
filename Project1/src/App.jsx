34dd67d09358d61d786b293508dbe11e91539ba2
Md-Asraful-Islam-Subbir <asraful17@cse.pstu.ac.bd>
2024-04-19 11:26:02 +0600

    react

---
+++ Project1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+import Navbar from "./components/Navigation/Navbar.jsx";
+import HeroSection from "./components/Navigation/Hero.jsx";
+function App() {
+  return (
+    <>
+    <Navbar />
+    <HeroSection />
+    </>
+  )
+}
+
+export default App
